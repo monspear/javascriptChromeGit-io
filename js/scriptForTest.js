30584e0cd528aa247d0ba64c7b6108c16e4c0389
monspear <46128987+monspear@users.noreply.github.com>
2023-03-05 22:11:37 +0900

    Add files via upload

---
+++ js/scriptForTest.js
@@ -0,0 +1,18 @@
+let adviceCount = 10;
+const randomNumber = Math.round(Math.random() * adviceCount); // 랜덤 수를 통해 명언 출력 순서정하기
+
+
+
+function onClickTodoList(){ // todolist 클릭 제어
+    
+}
+function onClickWeather(){ // 날씨 클릭시 상세정보 보여주기
+
+}
+function onClickStringLinks(){ // 좌상단 Links 클릭
+
+}
+
+
+
+
